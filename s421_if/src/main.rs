3009063716ce34eb99cbeca90edaf255e540c1a1
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2025-01-22 19:03:28 -0300

    s421_if 22/01/2025

---
+++ s421_if/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+   se();
+}
+
+fn se(){
+    let numero = 0;
+    if numero > 0{
+        println!("o numero {} é positivo", numero);
+    } else if numero == 0{
+        println!("o numero {} é zero", numero);
+
+    }else{
+        println!("o numero {} é negativo", numero);
+
+    }
+}
