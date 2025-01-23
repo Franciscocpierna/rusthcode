eb0ace32a1c2cb55d9fea1a8812e1cd4e8e536d7
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2025-01-23 10:32:42 -0300

    s424_While 23/01/2025

--- s424_While/src/main.rs
+++ s424_While/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    while_infinito();
+}
+
+
+
+
+
+fn while_infinito(){
+   let  mut contador = 0;
+   
+   while contador < 5{
+    println!("Contador igual {}", contador);
+    contador +=1;
+    
+   } 
 }
